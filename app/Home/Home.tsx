4e6b5d2a6b69d40fb3c40d08e780e14846a2b133
Dan Wehrle <dan@stellationcare.com>
2024-03-04 11:29:40 -0700

    Create grid

--- app/Home/Home.tsx
+++ app/Home/Home.tsx
@@ -1,3 +1,27 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import Square from './Square/Square';
+import './home.scss';
+
 export default function Home(): JSX.Element {
-	return <></>;
+	return (
+		<>
+			<Container className="text-center">
+				<Row className="justify-content-center">
+					<Square />
+					<Square />
+					<Square />
+				</Row>
+				<Row className="justify-content-center">
+					<Square />
+					<Square />
+					<Square />
+				</Row>
+				<Row className="justify-content-center">
+					<Square />
+					<Square />
+					<Square />
+				</Row>
+			</Container>
+		</>
+	);
 }
---
+++ app/Home/Square/Square.tsx
@@ -0,0 +1,5 @@
+import './square.scss';
+
+export default function Square(): JSX.Element {
+	return <div className="square" />;
+}
